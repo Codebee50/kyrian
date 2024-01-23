242a54a6201a98f795dba376d3878e9e6697f50e
Code bee <codebee345@outlook.com>
2024-01-23 21:06:44 +0100

    made projects section responsive

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+class Projects {
+    constructor(name, lanugages){
+        this.name =name;
+        this.lanugages = lanugages;
+    }
+}
+
+
+
 document.addEventListener('DOMContentLoaded', function(e){
     const mobile = document.getElementById('mobile')
     const closeNav = document.querySelector('.close-nav')
@@ -10,7 +19,19 @@ document.addEventListener('DOMContentLoaded', function(e){
     closeNav.addEventListener('click', function(e){
         hideNavBar()
     })
+
+    listOfProjects = populateProjectList()
+    var languagesbars = document.querySelectorAll(".languages-bar");
+    languagesbars.forEach(function(lanuguagebar){
+        barid = lanuguagebar.id
+        setupLanugageBar(barid)
+    })
 })
+
+function setupLanugageBar(barid){
+    let lanugageBar = document.getElementById(barid)
+    console.log(lanugageBar.offsetWidth)
+}
 
 function showNavBar(){
     const blurrables = document.querySelectorAll('.blurrable')
@@ -22,6 +43,22 @@ function showNavBar(){
     const navBar = document.querySelector('.nav')
     navBar.classList.add('visible')
    
+}
+
+function populateProjectList(){
+    let productList = []
+    let datagridderskills = {
+        "SCSS": 29,
+        "CSS": 27.4,
+        "HTML": 12.9,
+        "Python": 10.9,
+        "JavaScript": 10.8
+    }
+
+    let datagridder = new populateProjectList('datagridder', datagridderskills)
+    productList.push(datagridder)
+
+    return productList
 }
 
 function hideNavBar(){
