4116502963da0208015ac540b4f501f85cd03d0b
Code bee <codebee345@outlook.com>
2024-03-15 17:20:00 +0100

    changes

--- js/app.js
+++ js/app.js
@@ -1,35 +1,31 @@
-const projectsContainer = document.querySelector('.projects-container')
+const projectsContainer = document.querySelector(".projects-container");
 
+document.addEventListener("DOMContentLoaded", function (e) {
+  const mobile = document.getElementById("mobile");
+  const closeNav = document.querySelector(".close-nav");
 
-document.addEventListener('DOMContentLoaded', function(e){
-    const mobile = document.getElementById('mobile')
-    const closeNav = document.querySelector('.close-nav')
+  mobile.addEventListener("click", function (e) {
+    showNavBar();
+  });
 
+  closeNav.addEventListener("click", function (e) {
+    hideNavBar();
+  });
 
-    mobile.addEventListener('click', function(e){
-        showNavBar()
-    })
+  // fetch('data.json')
+  // .then(response => response.json())
+  // .then(data=> {
+  //     populateProjectsUi(data.projects)
+  // })
+});
 
-    closeNav.addEventListener('click', function(e){
-        hideNavBar()
-    })
+const randomInt = (min, max) =>
+  Math.trunc(Math.random() * (max - min) + 1) + min;
 
-    fetch('data.json')
-    .then(response => response.json())
-    .then(data=> {
-        populateProjectsUi(data.projects)
-    })
-
-    
-})
-
-const randomInt = (min,max) => Math.trunc(Math.random() * (max-min)+1) + min
-
-
-function populateProjectsUi(projects){
-    projects.forEach(function(project){
-        console.log(project)
-        const projectItem= `<div class="project-item">
+function populateProjectsUi(projects) {
+  projects.forEach(function (project) {
+    console.log(project);
+    const projectItem = `<div class="project-item">
         <div class="project-image-container">
             <img src="${project.img}" alt="Project image" class="project-image" loading="lazy">
         </div>
@@ -51,58 +47,52 @@ function populateProjectsUi(projects){
                 </div>
             </div>
         </div>
-    </div>`   
+    </div>`;
 
-    projectsContainer.insertAdjacentHTML("beforeend", projectItem)
+    projectsContainer.insertAdjacentHTML("beforeend", projectItem);
 
-    const projectLanguages = document.querySelector(`.languages-${project.id}`)
-    project.languages.forEach(function(language){
-        const languageHTML = `<div class="language-item">
-        <span class="circle-sm lang-sm circle-sm-bg-${randomInt(1,4)}" ></span>
+    const projectLanguages = document.querySelector(`.languages-${project.id}`);
+    project.languages.forEach(function (language) {
+      const languageHTML = `<div class="language-item">
+        <span class="circle-sm lang-sm circle-sm-bg-${randomInt(1, 4)}" ></span>
         <p>${language}</p>
     </div>
-`
-    if(projectLanguages){
-        projectLanguages.insertAdjacentHTML('beforeend', languageHTML)
-    }
-
-
-    })
-})
-  
+`;
+      if (projectLanguages) {
+        projectLanguages.insertAdjacentHTML("beforeend", languageHTML);
+      }
+    });
+  });
 }
 
+function showNavBar() {
+  const blurrables = document.querySelectorAll(".blurrable");
+  blurrables.forEach((blurrable) => {
+    blurrable.classList.add("blurred");
+  });
 
-function showNavBar(){
-    const blurrables = document.querySelectorAll('.blurrable')
-    blurrables.forEach((blurrable)=>{
-        blurrable.classList.add('blurred')
-    })
-
-
-    const navBar = document.querySelector('.nav')
-    navBar.classList.add('visible')
-   
+  const navBar = document.querySelector(".nav");
+  navBar.classList.add("visible");
 }
 
-function hideNavBar(){
-    const navBar = document.querySelector('.nav')
-    navBar.classList.remove('visible')
+function hideNavBar() {
+  const navBar = document.querySelector(".nav");
+  navBar.classList.remove("visible");
 
-    const blurrables = document.querySelectorAll('.blurrable')
-    blurrables.forEach((blurrable)=>{
-        blurrable.classList.remove('blurred')
-    })
+  const blurrables = document.querySelectorAll(".blurrable");
+  blurrables.forEach((blurrable) => {
+    blurrable.classList.remove("blurred");
+  });
 }
 
-function blurBlurrables(){
-    const blurrables = document.querySelectorAll('.blurrable')
-    blurrables.forEach((blurrable)=>{
-        blurrable.classList.add('blurred')
+function blurBlurrables() {
+  const blurrables = document.querySelectorAll(".blurrable");
+  blurrables.forEach((blurrable) => {
+    blurrable.classList.add("blurred");
 
-        blurrable.onclick = ()=>{
-            console.log('clicked on a blurrable')
-            hideNavBar()
-        }
-    })
+    blurrable.onclick = () => {
+      console.log("clicked on a blurrable");
+      hideNavBar();
+    };
+  });
 }
